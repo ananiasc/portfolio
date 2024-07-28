8c67575d4680148c019665c3c597323e1fd0be6c
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-28 15:21:08 -0300

    feat: applying two-column separation for resolutions like tablet and higher
    In these commits the header was also broken into two components, the header itself and a new one called resume-and-language-control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { ExperienceComponent } from "./experience/experience.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { WorkExperienceComponent } from "./work-experience/work-experience.component";
 import { EducationHistoryAndAboutMeComponent } from "./education-history-and-about-me/education-history-and-about-me.component";
+import { ResumeAndLanguageControlComponent } from "./resume-and-language-control/resume-and-language-control.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ExperienceComponent, PortfolioComponent, WorkExperienceComponent, EducationHistoryAndAboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, ResumeAndLanguageControlComponent, ExperienceComponent, PortfolioComponent, WorkExperienceComponent, EducationHistoryAndAboutMeComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
---
+++ src/app/resume-and-language-control/resume-and-language-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-and-language-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './resume-and-language-control.component.html'
+})
+export class ResumeAndLanguageControlComponent {
+
+}
