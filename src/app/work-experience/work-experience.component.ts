e3ee40921e05e71939e2bc9b9cbd578f0bfaf98f
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-30 00:06:23 -0300

    feat: applying internationalization in work experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-work-experience',
   standalone: true,
-  imports: [],
+  imports: [ TranslateModule ],
   templateUrl: './work-experience.component.html'
 })
 export class WorkExperienceComponent {
