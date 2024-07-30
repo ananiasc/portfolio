e280e47f23d5ca830ce29b20a5ccb3b83db12a8b
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-30 00:06:51 -0300

    feat: applying internationalization in history and about me

--- src/app/education-history-and-about-me/education-history-and-about-me.component.ts
+++ src/app/education-history-and-about-me/education-history-and-about-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-education-history-and-about-me',
   standalone: true,
-  imports: [],
+  imports: [ TranslateModule ],
   templateUrl: './education-history-and-about-me.component.html'
 })
 export class EducationHistoryAndAboutMeComponent {
