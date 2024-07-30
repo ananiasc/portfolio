d5bb61ad034b920d88394cca48614024e238bce4
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-30 00:04:12 -0300

    feat: applying internationalization in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
