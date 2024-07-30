2e511d0ddce1003d418299b045ac983d1d6538ce
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-30 00:06:03 -0300

    feat: applying internationalization in portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [ TranslateModule ],
   templateUrl: './portfolio.component.html'
 })
 export class PortfolioComponent {
