e838d531b5b5c61dd3cfcf37ec7ed23f99f88aa0
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-30 00:05:42 -0300

    feat: applying internationalization in experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [ TranslateModule ],
   templateUrl: './experience.component.html'
 })
 export class ExperienceComponent {
