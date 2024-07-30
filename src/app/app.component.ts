527643a6f22bd262ac749c11a92112946e148551
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-30 00:03:21 -0300

    feat: configuring internationalization in the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,21 @@ import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { WorkExperienceComponent } from "./work-experience/work-experience.component";
 import { EducationHistoryAndAboutMeComponent } from "./education-history-and-about-me/education-history-and-about-me.component";
 import { ResumeAndLanguageControlComponent } from "./resume-and-language-control/resume-and-language-control.component";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ResumeAndLanguageControlComponent, ExperienceComponent, PortfolioComponent, WorkExperienceComponent, EducationHistoryAndAboutMeComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    ResumeAndLanguageControlComponent, 
+    ExperienceComponent, 
+    PortfolioComponent, 
+    WorkExperienceComponent, 
+    EducationHistoryAndAboutMeComponent,
+    TranslateModule
+  ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
