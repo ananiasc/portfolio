a729e5e6f1aba57a5113d1d42622c854ef7a6423
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-31 19:48:49 -0300

    feat: adding routerLink to the component and next to it an opacity effect

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-work-experience',
   standalone: true,
-  imports: [ TranslateModule ],
+  imports: [ TranslateModule, RouterLink ],
   templateUrl: './work-experience.component.html'
 })
 export class WorkExperienceComponent {
