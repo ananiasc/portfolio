137eed9581ce78c8f2cbb108224828932aba79f0
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-08-01 23:12:48 -0300

    feat: using the component as a modal

--- src/app/work-experience-detail/work-experience-detail.component.ts
+++ src/app/work-experience-detail/work-experience-detail.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-work-experience-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './work-experience-detail.component.html'
 })
 export class WorkExperienceDetailComponent {
+  showModal: boolean = false;
 
+  public toggleModal(){
+    this.showModal = !this.showModal;
+  }
 }
--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
+import { WorkExperienceDetailComponent } from "../work-experience-detail/work-experience-detail.component";
 
 @Component({
   selector: 'app-work-experience',
   standalone: true,
-  imports: [ TranslateModule, RouterLink ],
+  imports: [
+    TranslateModule, 
+    RouterLink, 
+    WorkExperienceDetailComponent
+  ],
   templateUrl: './work-experience.component.html'
 })
 export class WorkExperienceComponent {
+  @ViewChild(WorkExperienceDetailComponent) workDetail!: WorkExperienceDetailComponent;
 
+  toggleModal() {
+    this.workDetail.toggleModal();
+  }
 }
