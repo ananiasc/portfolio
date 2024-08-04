20e51c7b0274eae3a24bea5bab797368de9e2879
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-08-04 16:07:12 -0300

    feat: defining experience interface

--- src/app/work-experience-detail/work-experience-detail.component.ts
+++ src/app/work-experience-detail/work-experience-detail.component.ts
@@ -1,5 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+interface Experience {
+  title: string,
+  period: string,
+  companyName: string,
+  companyDescription: string,
+  experienceDescription: string,
+  skills: string
+}
 
 @Component({
   selector: 'app-work-experience-detail',
