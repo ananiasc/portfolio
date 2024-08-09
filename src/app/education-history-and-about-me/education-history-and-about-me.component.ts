882877827b6becfa54863336d6f3f47feb4951ce
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-08-08 22:33:47 -0300

    feat: calling modal when clicking on about me

--- src/app/education-history-and-about-me/education-history-and-about-me.component.ts
+++ src/app/education-history-and-about-me/education-history-and-about-me.component.ts
@@ -1,20 +1,27 @@
 import { Component, ViewChild } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { EducationHistoryDetailComponent } from '../education-history-detail/education-history-detail.component';
+import { AboutMeDetailComponent } from '../about-me-detail/about-me-detail.component';
 
 @Component({
   selector: 'app-education-history-and-about-me',
   standalone: true,
   imports: [
     TranslateModule, 
-    EducationHistoryDetailComponent
+    EducationHistoryDetailComponent,
+    AboutMeDetailComponent,
   ],
   templateUrl: './education-history-and-about-me.component.html'
 })
 export class EducationHistoryAndAboutMeComponent {
   @ViewChild(EducationHistoryDetailComponent) educationDetail!: EducationHistoryDetailComponent;
+  @ViewChild(AboutMeDetailComponent) aboutMeDetail!: AboutMeDetailComponent;
 
   toggleModal() {
     this.educationDetail.toggleModal();
+  }
+
+  toggleModalAboutMeDetail() {
+    this.aboutMeDetail.toggleModal();
   }
 }
