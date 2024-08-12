b2cc7703d84f45e40b215a0e8a04a75581aee293
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-08-11 22:02:25 -0300

    feat: creating portfolio-page

---
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-page.component.html',
+})
+export class PortfolioPageComponent {
+
+}
