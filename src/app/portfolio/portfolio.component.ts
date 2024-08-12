cd0a51ed22f8e9d29074445f6e4abf7ac9b3bb68
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-08-11 22:12:39 -0300

    feat: send to portfolio page

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ TranslateModule ],
+  imports: [ 
+    TranslateModule,
+    RouterLink,
+   ],
   templateUrl: './portfolio.component.html'
 })
 export class PortfolioComponent {
